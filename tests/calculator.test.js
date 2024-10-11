2cbcd10474f4d9463e30a329e8a0af6ca8ce78b0
sammywalden <swalden@umass.edu>
2024-10-11 13:18:37 -0400

    added sqareroot tests

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -17,3 +17,13 @@ describe('Calculator Calculation Functions', function() {
 
 
 // Add your tests here
+describe('Calculator Calculation Functions', function() {
+    const res1 = calculator.calculateSquareRoot(100);
+    it('should return 10', function() {
+        assert.strictEqual(res1, 10);
+    });
+    const res2 = calculator.calculateSquareRoot(1);
+    it('should return 1', function() {
+        assert.strictEqual(res1, 1);
+    });
+});
